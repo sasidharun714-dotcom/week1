db839960acf3f0c07e402850954e08c803ebad69
sasidharun714-dotcom <sasidharun714@gmail.com>
2026-04-13 14:35:31 +0530

    Added RocketLaunch program

---
+++ RocketLaunch.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RocketLaunch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter countdown value: ");
+        int counter = sc.nextInt();
+
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+
+        System.out.println("Rocket Launched!");
+    }
+}
